ff04ca0f7e8694a8545aae9ff8ed847b85a593f3
Jody Lent <jodylent@gmail.com>
2015-08-31 16:30:15 -0700

    commented the finder lines

--- manifests/site.pp
+++ manifests/site.pp
@@ -103,8 +103,8 @@ node default {
   include osx::finder::empty_trash_securely
   include osx::finder::enable_quicklook_text_selection
 #  include osx::finder::show_all_on_desktop
-  include osx::finder::show_hidden_files
-  include osx::finder::unhide_library
+#  include osx::finder::show_hidden_files
+#  include osx::finder::unhide_library
  
   include osx::dock::autohide
   include osx::dock::clear_dock
