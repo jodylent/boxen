ccd296fe3ae69c80121ef360cd7d7d989f187c4d
Jody Lent <jodylent@gmail.com>
2015-08-31 16:21:48 -0700

    I don't wanna see that crap on my desktop

--- manifests/site.pp
+++ manifests/site.pp
@@ -102,7 +102,7 @@ node default {
   include osx::global::expand_save_dialog 
   include osx::finder::empty_trash_securely
   include osx::finder::enable_quicklook_text_selection
-  include osx::finder::show_all_on_desktop
+#  include osx::finder::show_all_on_desktop
   include osx::finder::show_hidden_files
   include osx::finder::unhide_library
  
